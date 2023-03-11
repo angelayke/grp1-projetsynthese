922098ca06df8cee6d227ed9e38e09d1ff293982
Jean Oldor Saintus <josaintus@gmail.com>
2023-03-10 23:06:47 -0500

    Mise à jour dans login et tableau affichage demande de stage

--- gestion-stages/src/app/app-routing.module.ts
+++ gestion-stages/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { TableauAffichageDemandeStageComponent } from './tableau-affichage-demande-stage/tableau-affichage-demande-stage.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'tableau-affichage-demande-stage', component: TableauAffichageDemandeStageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gestion-stages/src/app/login/login.component.ts
+++ gestion-stages/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -21,11 +22,22 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onSubmit(): void {
-    if (this.loginForm.valid){
-      const username = this.loginForm.get('username')?.value;
-      const email = this.loginForm.get('email')?.value
+  // onSubmit(): void {
+  //   if (this.loginForm.valid){
+  //     const username = this.loginForm.get('username')?.value;
+  //     const email = this.loginForm.get('email')?.value
 
+  //   }
+  // }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid){
+      alert("Veuillez remplir les champs SVP!")
+
+
+    }
+    else{
+      this.router.navigate(["/tableau-affichage-demande-stage"])
     }
   }
 
