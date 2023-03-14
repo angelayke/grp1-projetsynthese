5833024362d432c9ac7f1eb8bc02c51d45c5e7f9
angelayke <angebee2605@aol.com>
2023-03-13 22:01:57 -0400

    il manquait une virgule dans le app module- encore

--- gestion-stages/src/app/app.module.ts
+++ gestion-stages/src/app/app.module.ts
@@ -36,7 +36,7 @@ import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent,
-    TableauAffichageOffreStageComponent
+    TableauAffichageOffreStageComponent,
     HeaderComponent,
     AjoutCandidatComponent,
     CandidatsComponent,
