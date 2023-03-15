6148bf60c2ff8ac0d9a27b7cb3273fa72a27faca
angelayke <angebee2605@aol.com>
2023-03-14 22:05:49 -0400

    page entreprise suite, html css

--- gestion-stages/src/app/mock-entreprises.ts
+++ gestion-stages/src/app/mock-entreprises.ts
@@ -15,7 +15,53 @@ export const ENTREPRISES: Entreprise[] = [
         province: "Za pouwirag.",
         postalCode: "Vic kotdonfim.",
         published: false
-    }
+    },
+    {
+      
+      createdAt: "2060-10-13T22:44:55.970Z",
+      updatedAt: "2118-11-26T12:53:16.428Z",
+      name: "Dodcanug mafurwu.",
+      description: "Suhbe jowwofo.",
+      imageUrl: "Fe guc.",
+      contactName: "Uh vumo.",
+      contactEmail: "Kejkacuh hitof.",
+      contactPhone: "Tim kokeb.",
+      address: "Lope dawce.",
+      city: "Suswimrec vosalehic.",
+      province: "Sujibada ma.",
+      postalCode: "Tud ki.",
+      published: false
+    },
+    {
+      createdAt: "2106-03-01T14:40:21.369Z",
+      updatedAt: "2096-06-23T11:54:07.393Z",
+      name: "Entreprise4",
+      description: "Pabihuwe zawoc.",
+      imageUrl: "Jifep naalo.",
+      contactName: "Vecrorel kahcet.",
+      contactEmail: "Mugisoh avora.",
+      contactPhone: "Goke go.",
+      address: "Pet lasov.",
+      city: "Rodec me.",
+      province: "Za pouwirag.",
+      postalCode: "Vic kotdonfim.",
+      published: false
+  },
+  {
+    createdAt: "2060-10-13T22:44:55.970Z",
+    updatedAt: "2118-11-26T12:53:16.428Z",
+    name: "Pyjama",
+    description: "Suhbe jowwofo.",
+    imageUrl: "Fe guc.",
+    contactName: "Uh vumo.",
+    contactEmail: "Kejkacuh hitof.",
+    contactPhone: "Tim kokeb.",
+    address: "Lope dawce.",
+    city: "Suswimrec vosalehic.",
+    province: "Sujibada ma.",
+    postalCode: "Tud ki.",
+    published: false
+  }
 
 ]
 
