eba77a815e99856eaecab6330b9d2cbec69bd881
angelayke <angebee2605@aol.com>
2023-03-17 22:19:25 -0400

    routes- probleme avec mat drawer container, creer ng container avec ng if pour afficher des sections selon le clic de la nav

--- gestion-stages/src/app/app-routing.module.ts
+++ gestion-stages/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { FormulaireStageComponent } from './formulaire-stage/formulaire-stage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableaudebordComponent } from './tableaudebord/tableaudebord.component';
+import { TableauAffichageDemandeStageComponent } from './tableau-affichage-demande-stage/tableau-affichage-demande-stage.component';
+import { TableauAffichageOffreStageComponent } from './tableau-affichage-offre-stage/tableau-affichage-offre-stage.component';
+import { CandidatsComponent } from './candidats/candidats.component';
+import { EntreprisesComponent } from './entreprises/entreprises.component';
 
 const routes: Routes = [
-
-
-// { path: ''}, //component: LoginComponent
-// { path: 'tableaudebord'}, //component: TableaudebordComponent
-// { path: 'demandesdestage'}, //component: DemandesdestageComponent
-// { path: 'offredestage'}, //component: OffredestageComponent
-// { path: 'candidats'}, //component: CandidatsComponent
-// { path: 'entreprises'}, //component: EntreprisesComponent
+ { path: '', component: TableaudebordComponent}, //component: LoginComponent
+ { path: 'tableaudebord', component: TableaudebordComponent},
+ { path: 'demandesdestage', component: TableauAffichageDemandeStageComponent}, 
+ { path: 'offredestage', component: TableauAffichageOffreStageComponent}, 
+ { path: 'candidats', component: CandidatsComponent}, 
+ { path: 'entreprises', component: EntreprisesComponent}, 
 // { path: 'fichedemandestage'}, //component: FicheDemandeStageComponent
 // { path: 'formulairestage'}, //component: FormulaireStageComponent
 // { path: 'ficheoffrestage'}, //component: FicheOffreStageComponent
