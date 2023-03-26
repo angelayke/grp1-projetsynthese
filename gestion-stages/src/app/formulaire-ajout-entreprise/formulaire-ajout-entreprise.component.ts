9268dce97881d917c205ea800ac7e63785fc92a8
angelayke <angebee2605@aol.com>
2023-03-26 17:22:34 -0400

    formulaire ajout entreprise html css presque ok

--- gestion-stages/src/app/formulaire-ajout-entreprise/formulaire-ajout-entreprise.component.ts
+++ gestion-stages/src/app/formulaire-ajout-entreprise/formulaire-ajout-entreprise.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-formulaire-ajout-entreprise',
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulaire-ajout-entreprise.component.scss']
 })
 export class FormulaireAjoutEntrepriseComponent implements OnInit {
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileAttr = 'Logo';
+
+  uploadFileEvt(imgFile: any) {
+    if (imgFile.target.files && imgFile.target.files[0]) {
+      this.fileAttr = '';
+      Array.from(imgFile.target.files).forEach((file: any) => {
+        this.fileAttr += file.name + ' - ';
+      });
+      // HTML5 FileReader API
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        let image = new Image();
+        image.src = e.target.result;
+        image.onload = (rs) => {
+          let imgBase64Path = e.target.result;
+        };
+      };
+      reader.readAsDataURL(imgFile.target.files[0]);
+      // Reset if duplicate image uploaded again
+      this.fileInput.nativeElement.value = '';
+    } else {
+      this.fileAttr = 'Logo';
+    }
+  }
 
   constructor() { }
 
