8e31ab8ce96ec6d6bb2a4c88640eb85993213c09
TamanacoMartinez <tamanacomb@gmail.com>
2023-04-03 22:14:08 -0400

    fiche-offre-stage-update

--- gestion-stages/src/app/fiche-offre-stage/fiche-offre-stage.component.ts
+++ gestion-stages/src/app/fiche-offre-stage/fiche-offre-stage.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { MatCard } from '@angular/material/card';
+// import { Component, OnInit } from '@angular/core';
+// import { MatCard } from '@angular/material/card';
+// import { MatDialog } from '@angular/material/dialog';
+// import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+
+import { Stage } from '../stage';  
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { STAGES } from '../mock-stages'; 
+import { StagesService } from '../services/stages.service'; 
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 
@@ -10,36 +18,68 @@ import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation
 })
 export class FicheOffreStageComponent implements OnInit {
 
+  public loading:boolean = false;
+  public stage: Stage = {} as Stage;
+  public errorMessage : string | null = null;
+
+
+  public stageId: string | null = null;
+
   editing: boolean = false;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private stageService: StagesService, private activatedRoute: ActivatedRoute, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param)=>{
+     this.stageId = param.get('_id')
+
+    });
+    if(this.stageId){
+      this.stageService.getStageById(this.stageId).subscribe((data) => {
+        this.loading = true;
+        this.stage = data;
+        this.loading = false;
+  }, (error) => {
+    this.errorMessage = error;
+    this.loading = false;
+
+  })
+
+ }
+
+}
+
+  public isNotEmpty() {
+    return Object.keys(this.stage).length > 0;
   }
 
   onDoneClick(){
     this.editing = false;
     alert("Fiche sauvegardée!");
- }
+  }
 
- onEditClick(){
-   this.editing = true;
-   alert("Fiche modifiée!");
- }
+  onEditClick(){
+  this.editing = true;
+  alert("Fiche modifiée!");
+  }
+
+// onCancelClick(){
+//   this.editing = false;
+//   alert("Fiche annulée!");
+// }
 
 
   onDeleteClick(): void {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '400px',
-      data: { message: 'Êtes-vous sûr de vouloir supprimer cette information ?' }
-    });
+  const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+    width: '400px',
+    data: { message: 'Êtes-vous sûr de vouloir supprimer cette information ?' }
+  });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        console.log("Fiche annulée!")
-      }
-    });
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      console.log("Fiche annulée!")
+    }
+  });
   }
-
 
 }
